f145894721d85430fd4067ed8614599416620ee6
Herookie7 <herookie@tensi.org>
2025-12-10 22:27:12 +0530

    Update logo component to use SVG file path instead of embedded base64

--- lib/utils/assets/svgs/logo.tsx
+++ lib/utils/assets/svgs/logo.tsx
@@ -1,21 +1,13 @@
 export function AppLogo() {
   return (
     <div className="flex items-center justify-center relative p-2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
+      <img
+        src="/assets/images/svgs/logo.svg"
+        alt="Logo"
         width="160"
         height="40"
-        viewBox="0 0 1000 250"
-      >
-        <image
-          xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAA+gAAAD6CAYAAAAyVW3pAAAQAElEQVR4Aex9B4AkR3n1+6q6e8LmeHd7OShLJNlgguBAICQBQgQJiRxFNmBy8E+wMcFgbIzJ2GCSQSKLDEZkA5IAAZJQQqCs0+XdndRd9b+vZ2dvdm/34u7d3l319ZvKVV+9ru6tV9UzZxCOwEBgIDAQGAgMBAYCA4GBwEBgIDAQGAgMnOQMnP8B"
-          x="0"
-          y="0"
-          width="1000"
-          height="250"
-        />
-      </svg>
+      />
     </div>
   );
 }
+
