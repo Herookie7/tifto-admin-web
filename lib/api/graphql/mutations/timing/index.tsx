e99eddc6499eef38e624e5bd3105b043dc35f67e
Herookie7 <herookie@tensi.org>
2025-12-12 22:54:11 +0530

    Fix: Add missing day name translations and fix GraphQL mutation type
    - Add missing day name translations (Monday, Tuesday, etc.) to en.json locale file
    - Fix UPDATE_TIMINGS GraphQL mutation type from [TimingsInput] to [TimingsInput!]!
    - Remove console.log statement from order-superadmin-columns.tsx
    
    This fixes the MISSING_MESSAGE errors for day names and the GraphQL type mismatch error.

--- lib/api/graphql/mutations/timing/index.tsx
+++ lib/api/graphql/mutations/timing/index.tsx
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const UPDATE_TIMINGS = gql`
-  mutation UpdateTimings($id: String!, $openingTimes: [TimingsInput]) {
+  mutation UpdateTimings($id: String!, $openingTimes: [TimingsInput!]!) {
     updateTimings(id: $id, openingTimes: $openingTimes) {
       _id
       openingTimes {
